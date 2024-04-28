768830f3087e6c14d60ef1853aafd075ff38576c
SANDREQSA22 <163672523+SANDREQSA22@users.noreply.github.com>
2024-04-28 12:33:37 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template,redirect, url_for
+
+
+
+app = Flask(__name__)
+
+
+tasks =["push-ups", "pull-ups", "bench", "deadlift", "squats"]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', tasks=tasks)
+
+
+
+@app.route('/add_task', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+        return redirect(url_for('home'))
+    return render_template('add_task.html' , tasks=tasks)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#wigni ar wamikitxavs :(
